b49b446d1498cc266f8010e13b9582d552e3a3cb
dakshhpanchal <dakshpanchal08@gmail.com>
2025-04-06 01:08:50 +0530

    lane segementation with object avoidance thoda sa

--- src/ashwini/script/waypoint.py
+++ src/ashwini/script/waypoint.py
@@ -11,10 +11,8 @@ class WaypointNavigator:
         
         # List of waypoints (x, y, yaw_deg)
         self.waypoints = [
-            (5.0, 5.0, 0),
-            (-5.0, 5.0, 0),
-            (-5.0, -5.0, 0),
-            (5.0, -5.0, 0)
+            (18.0, 4.0, 0)
+            
         ]
         
         self.current_waypoint_index = 0
